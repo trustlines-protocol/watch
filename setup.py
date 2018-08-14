97dea854c6b56449521eb34da8e6b2be5c48d667
Ralf Schmitt <ralf@systemexit.de>
2018-08-14 16:00:23 +0200

    prepare for PyPI release: long description, license
    read the long description from README.rst, use MIT as license

--- setup.py
+++ setup.py
@@ -2,12 +2,40 @@
 
 from setuptools import setup
 
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="trustlines-watch",
     packages=["tlwatch"],
     setup_requires=["setuptools_scm"],
+    description="monitor trustlines cluster with riemann",
+    long_description=long_description,
     use_scm_version=True,
     install_requires=["requests", "bernhard", "click"],
+    # Author details
+    author="Trustlines-Network",
+    author_email="",
+    license="MIT",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 2 - Pre-Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+
     entry_points="""
         [console_scripts]
         tlwatch=tlwatch.cli:cli
