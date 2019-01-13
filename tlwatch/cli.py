fa56d8942e52dc822b470c314ef53b6a58b86283
Ralf Schmitt <ralf@systemexit.de>
2019-01-13 23:49:05 +0100

    implement --version switch
    we already have the --version switch in tl-deploy and we plan to implement in
    the relay server. so, let's do this everywhere.

--- tlwatch/cli.py
+++ tlwatch/cli.py
@@ -1,13 +1,24 @@
 import click
+import pkg_resources
 import tlwatch.etherscan
 import tlwatch.jsonrpc
 import tlwatch.relay
 import tlwatch.psql
 
 
-@click.group()
-def cli():
-    pass
+def report_version():
+    click.echo(pkg_resources.get_distribution("trustlines-watch").version)
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version", help="Prints the version of the software", is_flag=True)
+@click.pass_context
+def cli(ctx, version):
+    if version:
+        report_version()
+    elif ctx.invoked_subcommand is None:
+        click.echo(ctx.get_help())
+        ctx.exit()
 
 
 cli.add_command(tlwatch.etherscan.etherscan)
